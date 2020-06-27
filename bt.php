79755977fed529445db5ed2687dfbb088d18bd5a
2001ritu <66665883+2001ritu@users.noreply.github.com>
2020-06-27 22:55:39 +0530

    Update bt.php

--- bt.php
+++ bt.php
@@ -6,7 +6,7 @@ $dataPoints = array();
 for($i = 0; $i < $limit; $i++){
   $y += rand(0, 20)-5; 
 
-  $time = (time() + 3600*$i )*1000;
+  $time = (time() + 120*$i )*1000;
 
   array_push($dataPoints, array("x" => $time, "y" => $y));
 }
